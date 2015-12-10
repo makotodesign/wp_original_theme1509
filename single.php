33772f712f0974bb02e2bd446cc0c12c5d64ee62
703koushi <703manabu@gmail.com>
2015-12-10 13:59:06 +0900

    single.php added

--- single.php
+++ single.php
@@ -5,8 +5,12 @@
 				
 					<?php if(have_posts()):while(have_posts()):the_post();?>
 						<article class="content_sentence">	
-							<a  class="single_link" href="<?php the_permalink( )?>"><h2 class="content_title"><?php the_title(); ?></h2></a>					
+							<h2 class="content_title"><?php the_title(); ?></h2>					
+							<?php the_date('Y-m-d', '<p>', '</p>'); ?>
 							<div class="content_detail"><?php the_content(); ?></div>
+							<div class="cat">
+								<?php the_category(); ?>
+							</div>
 						</article>		
 					<?php endwhile; endif; ?>
 		
