7c92cdaa164310e679cd3dc62e0760acefe88388
703koushi <703manabu@gmail.com>
2015-12-11 11:00:29 +0900

    ウィジェットfunctions.phpで有効か

--- functions.php
+++ functions.php
@@ -5,5 +5,15 @@ register_nav_menus(array(
 )); 
 
 
+register_sidebar(array(
+	'name'=>'toppage',
+	'id'=>'toppage',
+	'before_widget'=>'<div class="widget">',
+	'after_widget'=>'</div>',
+	'before_title'=>'<h3>',
+	'after_title'=>'</h3>'
+));
+
+
 
 
